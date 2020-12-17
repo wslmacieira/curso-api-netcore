af8469409a2c5cf1a82d1827dd1ac2a77b02e945
wslmacieiradev <wslmacieira@outlook.com>
2020-12-17 00:59:11 -0300

    cria LoginService e ILoginService

---
+++ src/Api.Service/Services/LoginService.cs
@@ -0,0 +1,28 @@
+using System.Threading.Tasks;
+using Api.Domain.Entities;
+using Api.Domain.Interfaces.Services.User;
+using Api.Domain.Repository;
+
+namespace Api.Service.Services
+{
+    public class LoginService : ILoginService
+    {
+        private IUserRepository _repository;
+        public LoginService(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<object> findByLogin(UserEntity user)
+        {
+            if (user != null && string.IsNullOrWhiteSpace(user.Email))
+            {
+                return await _repository.FindByLogin(user.Email);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}

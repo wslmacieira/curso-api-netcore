fe85464e9d30a671e1ba796b30499fabda013bbe
wslmacieiradev <wslmacieira@outlook.com>
2020-12-21 15:17:48 -0300

    ajuste UsuarioCrudCompleto

--- src/Api.Data.Test/UsuarioCrudCompleto.cs
+++ src/Api.Data.Test/UsuarioCrudCompleto.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Api.Data.Context;
 using Api.Data.Implementations;
@@ -35,6 +36,32 @@ namespace Api.Data.Test
                 Assert.Equal(_entity.Email, _registroCriado.Email);
                 Assert.Equal(_entity.Name, _registroCriado.Name);
                 Assert.False(_registroCriado.Id == Guid.Empty);
+
+                _entity.Name = Faker.Name.First();
+                var _registroAtualizado = await _repository.UpdateAsync(_entity);
+                Assert.NotNull(_registroCriado);
+                Assert.Equal(_entity.Email, _registroAtualizado.Email);
+                Assert.Equal(_entity.Name, _registroAtualizado.Name);
+
+                var _registroExists = await _repository.ExistAsync(_registroAtualizado.Id);
+                Assert.True(_registroExists);
+
+                var _registroSelecionado = await _repository.SelectAsync(_registroAtualizado.Id);
+                Assert.NotNull(_registroSelecionado);
+                Assert.Equal(_entity.Email, _registroSelecionado.Email);
+                Assert.Equal(_entity.Name, _registroSelecionado.Name);
+
+                var _todosRegistros = await _repository.SelectAsync();
+                Assert.NotNull(_registroSelecionado);
+                Assert.True(_todosRegistros.Count() > 1);
+
+                var _removeu = await _repository.DeleteAsync(_registroSelecionado.Id);
+                Assert.True(_removeu);
+
+                var _usuarioPadrao = await _repository.FindByLogin("wslmacieira@gmail.com");
+                Assert.NotNull(_usuarioPadrao);
+                Assert.Equal("wslmacieira@gmail.com", _usuarioPadrao.Email);
+                Assert.Equal("Administrador", _usuarioPadrao.Name);
             }
 
         }

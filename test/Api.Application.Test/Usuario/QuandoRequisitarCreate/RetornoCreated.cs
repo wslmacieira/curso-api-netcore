8f4323959cd07ac5206b6f6d8fd2634d20600c65
wslmacieiradev <wslmacieira@outlook.com>
2020-12-25 17:56:41 -0300

    .gitignore Fixed

---
+++ test/Api.Application.Test/Usuario/QuandoRequisitarCreate/RetornoCreated.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Threading.Tasks;
+using Api.Domain.Dtos.User;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using src.Api.Application.Controllers;
+using src.Api.Domain.Interfaces.Services.User;
+using Xunit;
+
+namespace Api.Application.Test.Usuario.QuandoRequisitarCreate
+{
+    public class RetornoCreated
+    {
+        private UsersController _controller;
+
+        [Fact(DisplayName = "É Possivel Realizar o Created")]
+        public async Task E_Possivel_Invocar_a_Controller_Create()
+        {
+            var serviceMock = new Mock<IUserService>();
+            var nome = Faker.Name.FullName();
+            var email = Faker.Internet.Email();
+
+            serviceMock.Setup(m => m.Post(It.IsAny<UserDtoCreate>())).ReturnsAsync(
+                new UserDtoCreateResult
+                {
+                    Id = Guid.NewGuid(),
+                    Name = nome,
+                    Email = email,
+                    CreateAt = DateTime.UtcNow
+                }
+            );
+
+            _controller = new UsersController(serviceMock.Object);
+
+            Mock<IUrlHelper> url = new Mock<IUrlHelper>();
+            url.Setup(x => x.Link(It.IsAny<string>(), It.IsAny<object>())).Returns("http://localhost:5000");
+            _controller.Url = url.Object;
+
+            var userDtoCreate = new UserDtoCreate
+            {
+                Name = nome,
+                Email = email,
+            };
+
+            var result = await _controller.Post(userDtoCreate);
+            Assert.True(result is CreatedResult);
+
+            var resultValue = ((CreatedResult)result).Value as UserDtoCreateResult;
+            Assert.NotNull(resultValue);
+            Assert.Equal(userDtoCreate.Name, resultValue.Name);
+            Assert.Equal(userDtoCreate.Email, resultValue.Email);
+        }
+    }
+}

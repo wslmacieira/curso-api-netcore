db861820aeaab43f9e81a549f8bfb82b7c588cd2
wslmacieiradev <wslmacieira@outlook.com>
2020-12-26 11:30:07 -0300

    implementa Teste UserController

---
+++ test/Api.Application.Test/Usuario/QuandoRequisitadoUpdate/RetornoBadRequest.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Threading.Tasks;
+using Api.Domain.Dtos.User;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using src.Api.Application.Controllers;
+using src.Api.Domain.Interfaces.Services.User;
+using Xunit;
+
+namespace Api.Application.Test.Usuario.QuandoRequisitarUpdate
+{
+    public class RetornoBadRequest
+    {
+        private UsersController _controller;
+
+        [Fact(DisplayName = "É Possivel Realizar o Update")]
+        public async Task E_Possivel_Invocar_a_Controller_Update()
+        {
+            var serviceMock = new Mock<IUserService>();
+            var nome = Faker.Name.FullName();
+            var email = Faker.Internet.Email();
+
+            serviceMock.Setup(m => m.Put(It.IsAny<UserDtoUpdate>())).ReturnsAsync(
+                new UserDtoUpdateResult
+                {
+                    Id = Guid.NewGuid(),
+                    Name = nome,
+                    Email = email,
+                    UpdateAt = DateTime.UtcNow
+                }
+            );
+
+            _controller = new UsersController(serviceMock.Object);
+            _controller.ModelState.AddModelError("Email", "É um campo obrigatório");
+
+            var userDtoUpdate = new UserDtoUpdate
+            {
+                Name = nome,
+                Email = email,
+            };
+
+            var result = await _controller.Put(userDtoUpdate);
+            Assert.True(result is BadRequestObjectResult);
+            Assert.False(_controller.ModelState.IsValid);
+        }
+    }
+}

caadfee72b75112e322be0abacc22bd7ad129634
wslmacieiradev <wslmacieira@outlook.com>
2021-01-03 10:42:04 -0300

    Implementa Mapping das Entities

---
+++ src/Api.Data/Mapping/CepMap.cs
@@ -0,0 +1,21 @@
+using System;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data.Mapping
+{
+    public class CepMap : IEntityTypeConfiguration<CepEntity>
+    {
+        public void Configure(EntityTypeBuilder<CepEntity> builder)
+        {
+            builder.ToTable("Cep");
+
+            builder.HasKey(u => u.Id);
+
+            builder.HasIndex(u => u.Cep);
+
+            builder.HasOne(c => c.Municipio).WithMany(c => c.Ceps);
+        }
+    }
+}

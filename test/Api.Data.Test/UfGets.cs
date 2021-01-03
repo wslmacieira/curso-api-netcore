b72d26bc4d752dbd86890c0135ffb11b82e4c429
wslmacieiradev <wslmacieira@outlook.com>
2021-01-03 12:25:05 -0300

    Implementa testes unitarios de Municipio

---
+++ test/Api.Data.Test/UfGets.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Data.Context;
+using Data.Implementations;
+using Domain.Entities;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Api.Data.Test
+{
+    public class UfGets : BaseTest, IClassFixture<DbTeste>
+    {
+        private ServiceProvider _serviceProvider;
+
+        public UfGets(DbTeste dbTeste)
+        {
+            _serviceProvider = dbTeste.ServiceProvider;
+        }
+
+        [Fact(DisplayName = "Gets de UF")]
+        [Trait("GETs", "UfEntity")]
+        public async Task E_Possivel_Realizar_Gets_UF()
+        {
+            using (var context = _serviceProvider.GetService<Mycontext>())
+            {
+                UfImplementation _repository = new UfImplementation(context);
+                UfEntity _entity = new UfEntity
+                {
+                    Id = new Guid("e7e416de-477c-4fa3-a541-b5af5f35ccf6"),
+                    Sigla = "SP",
+                    Nome = "São Paulo"
+                };
+
+                var _registroExiste = await _repository.ExistAsync(_entity.Id);
+                Assert.True(_registroExiste);
+
+                var _registroSelecionado = await _repository.SelectAsync(_entity.Id);
+                Assert.NotNull(_registroExiste);
+                Assert.Equal(_entity.Sigla, _registroSelecionado.Sigla);
+                Assert.Equal(_entity.Nome, _registroSelecionado.Nome);
+                Assert.Equal(_entity.Id, _registroSelecionado.Id);
+
+                var _todosRegistros = await _repository.SelectAsync();
+                Assert.NotNull(_todosRegistros);
+                Assert.True(_todosRegistros.Count() == 27);
+            }
+        }
+    }
+}

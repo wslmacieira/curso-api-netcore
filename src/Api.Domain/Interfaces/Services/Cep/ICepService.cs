f158bde765d53b850fdc09ddd9c7dcd2bb58a079
wslmacieiradev <wslmacieira@outlook.com>
2021-01-03 10:07:40 -0300

    Implementa Interfaces de Cep Municipio e Uf

---
+++ src/Api.Domain/Interfaces/Services/Cep/ICepService.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Threading.Tasks;
+using Domain.Dtos.Cep;
+
+namespace Domain.Interfaces.Services.Cep
+{
+    public interface ICepService
+    {
+        Task<CepDto> Get(Guid id);
+        Task<CepDto> Get(string cep);
+        Task<CepDtoCreateResult> Post(CepDtoCreate cep);
+        Task<CepDtoUpdateResult> Put(CepDtoUpdate cep);
+        Task<bool> Delete(Guid id);
+    }
+}
---
+++ src/Api.Domain/Interfaces/Services/Uf/IUfService.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain.Dtos.Uf;
+
+namespace Domain.Interfaces.Services.Uf
+{
+    public interface IUfService
+    {
+        Task<UfDto> GetTask(Guid id);
+        Task<IEnumerable<UfDto>> GetAll();
+    }
+}

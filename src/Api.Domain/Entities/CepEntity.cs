d24f899a612d47b5830a902d65efe81248aad514
wslmacieiradev <wslmacieira@outlook.com>
2021-01-03 00:10:28 -0300

    cria CepEntity MunicipioEntity e UfEntity

---
+++ src/Api.Domain/Entities/CepEntity.cs
@@ -0,0 +1,25 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using Api.Domain.Entities;
+
+namespace Domain.Entities
+{
+    public class CepEntity : BaseEntity
+    {
+        [Required]
+        [MaxLength(10)]
+        public string Cep { get; set; }
+
+        [Required]
+        [MaxLength(60)]
+        public string Logradouro { get; set; }
+
+        [MaxLength(10)]
+        public string Numero { get; set; }
+
+        [Required]
+        public Guid MunicipioId { get; set; }
+
+        public MunicipioEntity Municipio { get; set; }
+    }
+}
---
+++ src/Api.Domain/Entities/UfEntity.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Api.Domain.Entities;
+
+namespace Domain.Entities
+{
+    public class UfEntity : BaseEntity
+    {
+        [Required]
+        [MaxLength(2)]
+        public string Sigla { get; set; }
+
+        [Required]
+        [MaxLength(45)]
+        public string Nome { get; set; }
+
+        public IEnumerable<MunicipioEntity> Municipios { get; set; }
+    }
+}

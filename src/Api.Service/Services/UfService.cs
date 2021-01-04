77e7906cdb677219944e0123f6594878ea2e2ed2
wslmacieiradev <wslmacieira@outlook.com>
2021-01-03 23:18:10 -0300

    Implementa services Uf Municipio e Cep

---
+++ src/Api.Service/Services/UfService.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Domain.Dtos.Uf;
+using Domain.Interfaces.Services.Uf;
+using Domain.Repository;
+
+namespace Service.Services
+{
+    public class UfService : IUfService
+    {
+        private IUfRepository _repository;
+        private readonly IMapper _mapper;
+
+        public UfService(IUfRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<UfDto> GetTask(Guid id)
+        {
+            var entity = await _repository.SelectAsync(id);
+            return _mapper.Map<UfDto>(entity);
+        }
+
+        public async Task<IEnumerable<UfDto>> GetAll()
+        {
+            var listEntity = await _repository.SelectAsync();
+            return _mapper.Map<IEnumerable<UfDto>>(listEntity);
+        }
+    }
+}
